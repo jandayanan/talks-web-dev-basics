602c75ae19785335a0952ef1480ad879085819ae
Jan Koichi Dayanan <jandayanan@gmail.com>
2021-06-16 23:06:52 +0800

    Jan > Initial COmmit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() { 
+    // this function runs when the DOM is ready, i.e. when the document has been parsed
+    document.getElementById("user-greeting").textContent = "Welcome back, Jan";
+});
